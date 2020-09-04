4afcbf62a08100e63d706fef70f2ded6d5980dc0
Shannon Duffy <sduffy@colgate.edu>
2020-09-04 13:14:40 -0400

    Experimented with functions and creating vectors

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,23 @@
 print("Hello World")
+#calculating 6 to the 6th power
+6^6
+#6 times 2 plus 5
+6*2+5
+#lets see if PEMDAS works
+5+6*2
+#it does
+#assigning a variable
+k<-4.03
+k*5.2
+#make a vector
+g<-c(5.08,3.94,8.02,6.91)
+g*k
+#question two, make some more vectors
+WormLengthmm<-c(3.29,4.98,1.00,2.87,7.65)
+WormMassg<-c(0.45,0.98,1.23,0.55,1.01)
+SoilMoisturePct<-c(45.6,38.9,12.4,87.3,62.1)
+HumusDepthcm<-c(3.28,15.20,6.83,11.72,8.21)
+#question 2, calculations
+WormMassg/WormLengthmm
+HumusDepthcm^2
+1/SoilMoisturePct
