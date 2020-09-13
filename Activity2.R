09fba98db7ff0a11ef211c268eb05ccf7af3ed33
Shannon Duffy <sduffy@colgate.edu>
2020-09-13 16:00:06 -0400

    Made more explicit comments, removed tutorial code, removed code to visualize data, numbered questions, made example integer vector using as.integer instead of putting "L" after each value, changed example factor vector to replace original character vector instead of creating a new one, fixed the main title of a histogram by typing name rather than using paste function

--- Activity2.R
+++ Activity2.R
@@ -1,51 +1,27 @@
-#Activity 2 tutorial
-#make a vector of tree heights in meters
-heights<-c(30,41,20,22)
-#convert to cm
-heights_cm<-heights*100
-#look at first tree height
-heights[1]
-help(matrix)
-#make a matrix
-mat<-matrix(c(1,2,3,4,5,6), ncol = 2, byrow = TRUE)
-#view matrix
-mat
-#make a matrix by column instead of row
-mat.bycol<-matrix(c(1,2,3,4,5,6), ncol = 2, byrow = FALSE)
-#view new matrix
-mat.bycol
-#look at column 2
-mat.bycol[,2]
-#look at row 2
-mat.bycol[2,]
+#Activity 2
 #read in noaa data
 datw<- read.csv("data/noaa_weather/2011124.csv")
 #get info about data frame
 str(datw)
+#Question 1- there are 157,849 rows and 9 columns
 #reformat date
 datw$dateF <- as.Date(datw$DATE, "%Y-%m-%d")
 #add year column
 datw$year <-as.numeric(format(datw$dateF, "%y"))
 
-#investigating data modes
+#Question 2- investigating data modes
 #create numeric vector
 watervolume_ml<- c(12.3, 54.3, 2.9, 5.0, 38.6)
 #create integer vector
 speciescount<- c(1,3,3,4,2)
-#check mode
-typeof(speciescount)
 #force to integer
-speciescount<- c(1L,3L,3L,4L,2L)
-#check mode again
-typeof(speciescount)
+speciescount<- as.integer(speciescount)
 #create character vector
 sitename<- c("field","stream","woods","yard","hill")
 #create factor vector
 sex<- c("male","female","female","unknown","female")
 #force to factor
-sex_factor<- as.factor(sex)
-#check mode
-class(sex_factor)
+sex<- as.factor(sex)
 
 #manipulating NOAA data
 #find out site names
@@ -56,24 +32,28 @@ mean(datw$TMAX[datw$NAME == "ABERDEEN, WA US"], na.rm = TRUE)
 datw$TAVE <- datw$TMIN + ((datw$TMAX-datw$TMIN)/2)
 #aggregate sites and take mean temp
 averagetemp<- aggregate(datw$TAVE, by=list(datw$NAME), FUN="mean", na.rm=TRUE)
-averagetemp
 #change column names
 colnames(averagetemp)<-c("NAME","MAAT")
 #numbering location names
 datw$siteN<- as.numeric(as.factor(datw$NAME))
+#make a histogram for Aberdeen
 hist(datw$TAVE[datw$siteN==1],freq=FALSE,main= "ABERDEEN, WA US",xlab = "Average Daily Temperature (degrees C)",ylab = "Relative Frequency", col = "grey50",border = "white")
+
+#Question 3- look up and describe functions
 #looking up functions
 help("hist")
 help("paste")
 help("levels")
+
 #add mean line to histogram
 hist(datw$TAVE[datw$siteN==1],freq=FALSE, main = "ABERDEEN, WA US", xlab = "Average Daily Temperature (degrees C)",ylab = "Relative Frequency", col = "grey50",border = "white")
 abline(v=mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), col = "tomato3", lwd = 3)
 #add standard deviation below mean
 abline(v=mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE)-sd(datw$TAVE[datw$siteN == 1], na.rm= TRUE), col = "tomato3", lwd = 3, lty = 3)
-#add standard deviatoin above mean
+#add standard deviation above mean
 abline(v=mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE)+sd(datw$TAVE[datw$siteN == 1], na.rm= TRUE), col = "tomato3", lwd = 3, lty = 3)
-#make histograms for three other sites
+
+#Question 4- make histograms for three other sites
 #group 4 charts together
 par(mfrow=c(2,2))
 #copy site 1
@@ -98,14 +78,16 @@ abline(v=mean(datw$TAVE[datw$siteN == 4], na.rm = TRUE)-sd(datw$TAVE[datw$siteN 
 abline(v=mean(datw$TAVE[datw$siteN == 4], na.rm = TRUE)+sd(datw$TAVE[datw$siteN == 4], na.rm= TRUE), col = "tomato3", lwd = 3, lty = 3)
 
 #probability distribution
+#get just one plot per frame
 par(mfrow=c(1,1))
 h1 <- hist(datw$TAVE[datw$siteN == 1],
            freq=FALSE, 
-           main = paste(levels(datw$NAME)[1]),
+           main = "ABERDEEN, WA US",
            xlab = "Average daily temperature (degrees C)", 
            ylab="Relative frequency",
            col="grey50",
            border="white")
+#plot the normal distribution
 x.plot <- seq(-10,30, length.out = 100)
 y.plot <-  dnorm(seq(-10,30, length.out = 100),
                  mean(datw$TAVE[datw$siteN == 1],na.rm=TRUE),
@@ -117,8 +99,9 @@ points(x.plot,
        col = "royalblue3",
        lwd = 4, 
        lty = 2)
+
+#calculating probability
 #finding probability that daily average temp is below freezing
-help(dnorm)
 pnorm(0, mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), sd(datw$TAVE[datw$siteN == 1], na.rm = TRUE))
 #finding probability that daily average temp is below 5 degrees
 pnorm(5, mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), sd(datw$TAVE[datw$siteN == 1], na.rm = TRUE))
@@ -126,22 +109,30 @@ pnorm(5, mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), sd(datw$TAVE[datw$siteN
 pnorm(5, mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), sd(datw$TAVE[datw$siteN == 1], na.rm = TRUE))-pnorm(0, mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), sd(datw$TAVE[datw$siteN == 1], na.rm = TRUE))
 #finding probability that daily average temp is above 20 degrees
 1 - pnorm(20, mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), sd(datw$TAVE[datw$siteN == 1], na.rm = TRUE))
-#finding the temp above which ther is only a 5% chance of occurring
+#finding the temp above which there is only a 5% chance of occurring
 qnorm(0.95, mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), sd(datw$TAVE[datw$siteN == 1], na.rm = TRUE))
-#question 6: 4 degree increase in mean, what is probability above current high extreme
+
+#Question 6- What is the probability of current extreme temperatures if there is a 4 degree increase in mean
 1 - pnorm(qnorm(0.95, mean(datw$TAVE[datw$siteN == 1], na.rm = TRUE), sd(datw$TAVE[datw$siteN == 1], na.rm = TRUE)), mean(datw$TAVE[datw$siteN == 1] + 4, na.rm = TRUE), sd(datw$TAVE[datw$siteN == 1], na.rm = TRUE))
 
 #Evaluating Precipitation Data
-#Question 7 daily precip for Aberdeen histogram
+#Question 7- daily precip for Aberdeen histogram
 aberdeen_precip_hist<-hist(datw$PRCP[datw$siteN == 1], freq=FALSE, main = paste(levels(datw$NAME[4])), xlab = "Daily Precipitation (cm)",ylab = "Relative Frequency", col = "black",border = "white")
-#Question 8 yearly precipitation
+
+#Question 8- annual precipitation for each year and site
+#create new dataframe
 annualprecip<- aggregate(datw$PRCP, by=list(datw$NAME,datw$year), FUN="sum", na.rm = TRUE)
+#rename columns
 colnames(annualprecip)<-c("NAME","year","Annual_Precipitation")
+#create histogram for annual precipitation for Aberdeen
 aberdeen_annual_precip_hist<-hist(annualprecip$Annual_Precipitation[annualprecip$NAME == "ABERDEEN, WA US"], freq=FALSE, main = "ABERDEEN, WA US", xlab = "Annual Precipitation (cm)",ylab = "Relative Frequency", col = "grey50",border = "white")
+#create histograms for other sites to observe normality
 livermore_annual_precip_hist<-hist(annualprecip$Annual_Precipitation[annualprecip$NAME == "LIVERMORE, CA US"], freq=FALSE, main = "LIVERMORE, CA US", xlab = "Annual Precipitation (cm)",ylab = "Relative Frequency", col = "grey50",border = "white")
 morrisville_annual_precip_hist<-hist(annualprecip$Annual_Precipitation[annualprecip$NAME == "MORRISVILLE 6 SW, NY US"], freq=FALSE, main = "MORRISVILLE 6 SW, NY US", xlab = "Annual Precipitation (cm)",ylab = "Relative Frequency", col = "grey50",border = "white")
 mormonflat_annual_precip_hist<-hist(annualprecip$Annual_Precipitation[annualprecip$NAME == "MORMON FLAT, AZ US"], freq=FALSE, main = "MORMON FLAT, AZ US", xlab = "Annual Precipitation (cm)",ylab = "Relative Frequency", col = "grey50",border = "white")
 mandan_annual_precip_hist<-hist(annualprecip$Annual_Precipitation[annualprecip$NAME == "MANDAN EXPERIMENT STATION, ND US"], freq=FALSE, main = "MANDAN EXPERIMENT STATION, ND US", xlab = "Annual Precipitation (cm)",ylab = "Relative Frequency", col = "grey50",border = "white")
-#mean annual precip
+
+#Question 9- get mean annual precip for each site
 meanannualprecip<- aggregate(annualprecip$Annual_Precipitation, by=list(annualprecip$NAME), FUN="mean", na.rm = TRUE)
+#rename columns
 colnames(meanannualprecip)<-c("NAME","Mean_Annual_Precipitation")
