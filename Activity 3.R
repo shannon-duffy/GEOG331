2e542e2a673605481a41d3818b3c94516c109874
Shannon Duffy <sduffy@colgate.edu>
2020-09-24 01:44:21 -0400

    cleared out tutorial, commented question numbers, reconfigured plots for question 9

--- Activity 3.R
+++ Activity 3.R
@@ -1,20 +1,12 @@
-#Tutorial
-#creating a function
-assert<-function(statement, err.message){if(statement == FALSE){print(err.message)}}
-assert(1 == 2, "error: unequal values")
-assert(2 == 2, "error: unequal values")
-a <- c(1,2,3,4)
-b <- c(8,4,5)
-assert(length(a) == length(b), "error: unequal length")
+#Activity 3
 
+#Question 3: what's the difference between skip and nrows?
 #read in data
 datW<-read.csv("data/bewkes/bewkes_weather.csv", skip = 3, na.strings = c("#N/A"),header = FALSE)
 #make column names
 sensorInfo<-read.csv("data/bewkes/bewkes_weather.csv", na.strings = c("#N/A"), nrows = 2)
 colnames(datW)<- colnames(sensorInfo)
 
-
-#QA/QC
 #use lubridate package to reformat dates
 install.packages(c("lubridate"))
 library("lubridate")
@@ -25,18 +17,8 @@ datW$doy <- yday(dates)
 datW$hour <- hour(dates) + (minute(dates)/60)
 #calculate decimal day of year
 datW$DD <- datW$doy + (datW$hour/24)
-#quick preview of new date calcualtions
-datW[1,]
-#see how many values have missing data for each sensor observation
-#air temperature
-length(which(is.na(datW$air.temperature)))
-length(which(is.na(datW$wind.speed)))
-length(which(is.na(datW$precipitation)))
-length(which(is.na(datW$soil.moisture)))
-length(which(is.na(datW$soil.temp)))
-#plotting soil moisture over time to find out missing data
-plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
-     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+#Question 4: Is temp data reliable?
 #plor air temp over time to visually check irregularities
 plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
      ylab="Air temperature (degrees C)")
@@ -49,27 +31,18 @@ datW[datW$air.tempQ1 < 8,]
 #look at days with really high air temperature
 datW[datW$air.tempQ1 > 33,]  
 
-
 #plot precipitation and lightning strikes on the same plot
 #normalize lighting strikes to match precipitation
 lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
-#make the plot with precipitation and lightning activity marked
-#make it empty to start and add in features
-plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
-     type="n")
-#plot precipitation points only when there is precipitation 
-#make the points semi-transparent
-points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
-       col= rgb(95/255,158/255,160/255,.5), pch=15)
-#plot lightning points only when there is lightning     
-points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
-       col= "tomato3", pch=19)
-#use assert function to explain why lightscale can subset datW
+
+#Question 5: use assert function to explain why lightscale can subset datW
+assert<-function(statement, err.message){if(statement == FALSE){print(err.message)}}
 assert(length(lightscale) == length(datW$DD), "error: unequal length")
-#filter out storms in wind and air temperature measurements
-# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
-#create a new air temp column
+
+#create a new wind speed column filtering out storm days (2mm rain +lightning or >5mm rain)
 datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA, ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+
+#Question 6: do QA/QC for wind speed
 #create a new wind speed column filtering out storm days (2mm rain +lightning or >5mm rain)
 datW$wind.speedQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA, ifelse(datW$precipitation > 5, NA, datW$wind.speed))
 #use assert function to see if it filtered properly
@@ -77,7 +50,8 @@ assert(length(which(is.na(datW$wind.speedQ2))) > 0, "error: no NA values found")
 #plot wind speed over time with new wind speed data
 plot(datW$DD , datW$wind.speedQ2, xlab = "Day of Year", ylab = "Wind Speed", type = "b", pch=19)
 
-#check soil moisture sensor validity
+#Question 7: check soil sensor validity
+#check soil moisture validity
 #find extreme soil moisture values
 quantile(datW$soil.moisture, na.rm = TRUE)
 #find extreme precip values (excluding time with no rain)
@@ -95,10 +69,10 @@ datW[datW$soil.temp > 24,]
 #check to see if air temp values on low extreme soil temp times (>10) are also low
 datW[datW$soil.temp < 10,]
 
-#making a table of averages
+#Question 8:making a table of averages
 #mack object for each average rounded to the appropriate decimal
 #temp ends in 0 so use sprintf function to keep it
-avetemp<-sprintf("%.1f",round(mean(datW$air.tempQ2, na.rm = TRUE), digits = 1))
+avetemp<-sprintf("%.1f", mean(datW$air.tempQ2, na.rm = TRUE))
 avewind<-round(mean(datW$wind.speedQ2, na.rm = TRUE), digits = 2)
 avesoiltemp<-round(mean(datW$soil.temp, na.rm = TRUE), digits = 1)
 avesoilmoist<-round(mean(datW$soil.moisture, na.rm = TRUE), digits = 4)
@@ -114,3 +88,18 @@ nobs<- c(length(datW$air.tempQ2[!is.na(datW$air.tempQ2)]), length(datW$wind.spee
 nobstext<-c("n = 2105", "n = 2105", "n = 1411", "n = 1141", "n = 2188")
 #add row of number of observations to table of averages in new dataframe
 avetablenobs<-rbind(avetable,nobstext)
+
+#Question 9: making plots of weather variables
+#group together
+par(mfrow=c(2,2), mar=c(3,4,0.5,0), mgp=c(1.5,0.5,0))
+#air temp
+plot(datW$DD , datW$air.tempQ2, xlab = "Day of Year", ylab = "Air Temperature (°C)", type = "l")
+#precipitation
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation (mm)", type = "l")
+#soil temp
+plot(datW$DD , datW$soil.temp, xlab = "Day of Year", ylab = "Soil Temperature (°C)", type = "l")
+#soil moisture
+plot(datW$DD , datW$soil.moisture, xlab = "Day of Year", ylab = "Soil Moisture (m^3/m^3)", type = "l")
+
+
+
