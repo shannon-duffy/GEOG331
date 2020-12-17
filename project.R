8729ed07232292a83107d0645ed3d24cef64e879
Shannon Duffy <sduffy@colgate.edu>
2020-12-16 23:56:58 -0500

    final code

--- project.R
+++ project.R
@@ -15,14 +15,16 @@ station_name<-ncvar_get(ncar, ncar$var$stn_name)
 river_name<-ncvar_get(ncar, ncar$var$riv_name)
 plot(flow.kinshasa, type="l")
 
-#read in tree cover data
+#read in tree cover data and convert from m^2 to km^2
 coverstats<-read.csv('coverstats.csv')
-cover2000<-coverstats$treecover2000
+cover2000<-(coverstats$treecover2000)/100000000
 gainstats<-read.csv('gainstats.csv')
-gain2000_2019<-gainstats$gain
+gain2000_2019<-(gainstats$gain)/1000000
 gain2000_2019/cover2000
 #only 0.006% of forest area has been gained from 2000-2019
 lossstats<-read.csv('lossstats.csv')
+lossstats$Loss<-(lossstats$Loss)/1000000
+sum(lossstats$Loss)/cover2000
 
 #read in precip from World Clim
 library(raster)
@@ -31,7 +33,6 @@ library(rgdal)
 library(rgeos)
 library(plyr)
 library(tiff)
-install.packages("sf")
 library(sf)
 
 #set up months to read in
@@ -46,9 +47,6 @@ for(i in 1:length(precmonth)){
 
 #read in boundary shapefile
 congo_polygon<-readOGR("congoshape/congoshape.shp")
-#rasterize
-congoraster<-rasterize(congo_polygon,prec01.2000)
-plot(congoraster)
 
 
 precextract <- list()
@@ -60,44 +58,87 @@ for(i in 1:length(precmonth)){
   #calculate the mean of the NDVI values
   sumprec[i] <- sum(precextract[[i]], na.rm=TRUE)
 }
-View(sumprec)
 
 #create date variable
 dateprecmonth <- as.Date(paste(precmonth,"-01",sep=""))
 #create year variable
+library(lubridate)
 Year<-year(dateprecmonth)
 #add precip and discharge data to date and year in data frame
 congohydro<-data.frame(dateprecmonth,Year,sumprec,flow.kinshasa)
 #calculate SDR
 congohydro$SDR<-(flow.kinshasa/sumprec)
-#aggregate flow and SDR sum, mean, and standard deviation by year
+#aggregate flow and SDR min, max, mean, and standard deviation by year and get CV
 library(stats)
-flowsum<-stats::aggregate(congohydro$flow.kinshasa, by=list(congohydro$Year), FUN=sum)
-colnames(flowsum)<-c("Year","Annual Discharge")
+flowmin<-stats::aggregate(congohydro$flow.kinshasa, by=list(congohydro$Year), FUN=min)
+flowmax<-stats::aggregate(congohydro$flow.kinshasa, by=list(congohydro$Year), FUN=max)
 flowmean<-stats::aggregate(congohydro$flow.kinshasa, by=list(congohydro$Year), FUN=mean)
 flowsd<-stats::aggregate(congohydro$flow.kinshasa, by=list(congohydro$Year), FUN=sd)
-sdrsum<-stats::aggregate(congohydro$SDR, by=list(congohydro$Year), FUN=sum)
-colnames(sdrsum)<-c("Year","Annual SDR")
+flowcv<-flowmean$x/flowsd$x
+sdrmin<-stats::aggregate(congohydro$SDR, by=list(congohydro$Year), FUN=min)
+sdrmax<-stats::aggregate(congohydro$SDR, by=list(congohydro$Year), FUN=max)
 sdrmean<-stats::aggregate(congohydro$SDR, by=list(congohydro$Year), FUN=mean)
 sdrsd<-stats::aggregate(congohydro$SDR, by=list(congohydro$Year), FUN=sd)
-#get CV for flow and SDR
-flowcv<-data.frame(c(2000:2010),flowmean$x/flowsd$x)
-colnames(flowcv)<-c("Year","Discharge CV")
-sdrcv<-data.frame(c(2000:2010),sdrmean$x/sdrsd$x)
-colnames(sdrcv)<-c("Year","SDR CV")
-#join deforestation data to flow and sdr sum and cv data
-foresthydro1<-join(flowcv, lossstats, by="Year", type="left")
-foresthydro1<-join(foresthydro1,sdrcv, by="Year", type= "left")
-foresthydro1<-join(foresthydro1,flowsum, by="Year", type= "left")
-foresthydro1<-join(foresthydro1,sdrsum, by="Year", type= "left")
+sdrcv<-sdrmean$x/sdrsd$x
+hydrostats<-data.frame(flowmin$Group.1,flowmin$x,flowmax$x,flowmean$x,flowsd$x,flowcv,sdrmin$x,sdrmax$x,sdrmean$x,sdrsd$x,sdrcv)
+colnames(hydrostats)<-c("Year","Minimum Monthly Discharge", "Maximum Monthly Discharge","Mean Monthly Discharge","Monthly Discharge Standard Deviation","Monthly Discharge CV","Minimum Monthly SDR", "Maximum Monthly SDR","Mean Monthly SDR","Monthly SDR Standard Deviation","Monthly SDR CV")
+
+#monthly times series plots
+par(mar = c(4,5,3,1))
+plot(congohydro$dateprecmonth, congohydro$flow.kinshasa, type = "l", main = "Congo River Monitoring Station at Kinshasa", xlab = "Time", ylab = expression(paste("Monthly Discharge (m"^3,"/s)")))
+par(mar = c(4,4,3,1))
+plot(congohydro$dateprecmonth, congohydro$SDR, type = "l", main = "Congo River Monitoring Station at Kinshasa", xlab = "Time", ylab = "Monthly Specific Discharge Ratio")
+plot(congohydro$dateprecmonth, congohydro$sumprec, type = "l", main = "Congo River Monitoring Station at Kinshasa", xlab = "Time", ylab = "Monthly Rainfall (mm)")
+
+#join deforestation data to flow data
+foresthydro<-join(hydrostats, lossstats, by="Year", type="left")
+
 #make 2000 0
-foresthydro1[is.na(foresthydro1)]<-0
+foresthydro[is.na(foresthydro)]<-0
 #calculate remaining forest
-foresthydro1$cover<-cover2000-foresthydro1$Loss-cumsum(foresthydro1$Loss)
-#plot forest change against flow
-plot(foresthydro1$cover,foresthydro1$`Discharge CV`)
-plot(foresthydro1$cover,foresthydro1$`SDR CV`)
-plot(foresthydro1$Year,foresthydro1$`SDR CV`)
-#plot against raw values
-plot(foresthydro1$cover, foresthydro1$`Annual Discharge`)
-plot(foresthydro1$cover, foresthydro1$`Annual SDR`)
+foresthydro$cover<-cover2000-foresthydro$Loss-cumsum(foresthydro$Loss)
+
+#regressions and plots
+library(mblm)
+par(mar = c(4,5,3,1))
+mean.discharge.reg<-mblm(`Mean Monthly Discharge` ~ cover, data = foresthydro)
+plot(foresthydro$cover, foresthydro$`Mean Monthly Discharge`, main="Congo River Monitoring Station at Kinshasa", xlab = expression(paste("Forest Cover (km"^2,")")), ylab=expression(paste("Mean Discharge (m"^3,"/s)")))
+lines(c(min(foresthydro$cover),max(foresthydro$cover)),c(mean.discharge.reg$fitted.values[which.min(foresthydro$cover)],mean.discharge.reg$fitted.values[which.max(foresthydro$cover)]),col = "red")
+min.discharge.reg<-mblm(`Minimum Monthly Discharge` ~ cover, data = foresthydro)
+plot(foresthydro$cover, foresthydro$`Minimum Monthly Discharge`, main="Congo River Monitoring Station at Kinshasa", xlab = expression(paste("Forest Cover (km"^2,")")), ylab=expression(paste("Minimum Monthly Discharge (m"^3,"/s)")))
+lines(c(min(foresthydro$cover),max(foresthydro$cover)),c(min.discharge.reg$fitted.values[which.min(foresthydro$cover)],min.discharge.reg$fitted.values[which.max(foresthydro$cover)]),col = "red")
+max.discharge.reg<-mblm(`Maximum Monthly Discharge` ~ cover, data = foresthydro)
+plot(foresthydro$cover, foresthydro$`Maximum Monthly Discharge`, main="Congo River Monitoring Station at Kinshasa", xlab = expression(paste("Forest Cover (km"^2,")")), ylab=expression(paste("Maximum Monthly Discharge (m"^3,"/s)")))
+lines(c(min(foresthydro$cover),max(foresthydro$cover)),c(max.discharge.reg$fitted.values[which.min(foresthydro$cover)],max.discharge.reg$fitted.values[which.max(foresthydro$cover)]),col = "red")
+discharge.CV.reg<-mblm(`Monthly Discharge CV` ~ cover, data = foresthydro)
+plot(foresthydro$cover, foresthydro$`Monthly Discharge CV`, main="Congo River Monitoring Station at Kinshasa", xlab = expression(paste("Forest Cover (km"^2,")")), ylab=expression(paste("Monthly Discharge CV (m"^3,"/s)")))
+lines(c(min(foresthydro$cover),max(foresthydro$cover)),c(discharge.CV.reg$fitted.values[which.min(foresthydro$cover)],discharge.CV.reg$fitted.values[which.max(foresthydro$cover)]),col = "red")
+mean.SDR.reg<-mblm(`Mean Monthly SDR` ~ cover, data = foresthydro)
+plot(foresthydro$cover, foresthydro$`Mean Monthly SDR`, main="Congo River Monitoring Station at Kinshasa", xlab = expression(paste("Forest Cover (km"^2,")")), ylab="Mean SDR")
+lines(c(min(foresthydro$cover),max(foresthydro$cover)),c(mean.SDR.reg$fitted.values[which.min(foresthydro$cover)],mean.SDR.reg$fitted.values[which.max(foresthydro$cover)]),col = "red")
+min.SDR.reg<-mblm(`Minimum Monthly SDR` ~ cover, data = foresthydro)
+plot(foresthydro$cover, foresthydro$`Minimum Monthly SDR`, main="Congo River Monitoring Station at Kinshasa", xlab = expression(paste("Forest Cover (km"^2,")")), ylab="Minimum Monthly SDR")
+lines(c(min(foresthydro$cover),max(foresthydro$cover)),c(min.SDR.reg$fitted.values[which.min(foresthydro$cover)],min.SDR.reg$fitted.values[which.max(foresthydro$cover)]),col = "red")
+max.SDR.reg<-mblm(`Maximum Monthly SDR` ~ cover, data = foresthydro)
+plot(foresthydro$cover, foresthydro$`Maximum Monthly SDR`, main="Congo River Monitoring Station at Kinshasa", xlab = expression(paste("Forest Cover (km"^2,")")), ylab="Maximum Monthly SDR")
+lines(c(min(foresthydro$cover),max(foresthydro$cover)),c(max.SDR.reg$fitted.values[which.min(foresthydro$cover)],max.SDR.reg$fitted.values[which.max(foresthydro$cover)]),col = "red")
+SDR.CV.reg<-mblm(`Monthly SDR CV` ~ cover, data = foresthydro)
+plot(foresthydro$cover, foresthydro$`Monthly SDR CV`, main="Congo River Monitoring Station at Kinshasa", xlab = expression(paste("Forest Cover (km"^2,")")), ylab="Monthly SDR CV")
+lines(c(min(foresthydro$cover),max(foresthydro$cover)),c(SDR.CV.reg$fitted.values[which.min(foresthydro$cover)],SDR.CV.reg$fitted.values[which.max(foresthydro$cover)]),col = "red")
+
+summary(mean.discharge.reg)
+summary(min.discharge.reg)
+summary(max.discharge.reg)
+summary(discharge.CV.reg)
+summary(mean.SDR.reg)
+summary(min.SDR.reg)
+summary(max.SDR.reg)
+summary(SDR.CV.reg)
+
+
+#monthly times series plots
+par(mar = c(4,5,3,1))
+plot(congohydro$dateprecmonth, congohydro$flow.kinshasa, type = "l", main = "Congo River Monitoring Station at Kinshasa", xlab = "Time", ylab = expression(paste("Monthly Discharge (m"^3,"/s)")))
+par(mar = c(4,4,3,1))
+plot(congohydro$dateprecmonth, congohydro$SDR, type = "l", main = "Congo River Monitoring Station at Kinshasa", xlab = "Time", ylab = "Monthly Specific Discharge Ratio")
+                                                                                                      
